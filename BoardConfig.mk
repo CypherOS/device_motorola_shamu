a90252abcb6b6e5dbf3d27751dff5e18675abc5c
dankoman <dankoman30@gmail.com>
2014-12-16 04:00:06 +0100

    Shamu: Enable qcom time services
    Change-Id: I6dd909372b30a236e42acb7e8b269b68f487b5d0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,9 @@ USE_CLANG_PLATFORM_BUILD := true
 # Disable dex-preopt of prebuilts to save space.
 DONT_DEXPREOPT_PREBUILTS := true
 
+# Qualcomm Time Services
+BOARD_USES_QC_TIME_SERVICES := true
+
 TARGET_FS_CONFIG_GEN += device/motorola/shamu/config.fs
 
 -include vendor/motorola/shamu/BoardConfigVendor.mk
