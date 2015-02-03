40a3918707527672a5b203d59abbbe51d301d51a
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-02-02 19:09:35 -0800

    shamu: don't use ShamuLayout
    Change-Id: I1dd7655877e202f97532e7dc9cecaaa4dcb42319

--- device.mk
+++ device.mk
@@ -234,8 +234,8 @@ PRODUCT_PACKAGES += \
     charger_res_images
 
 # for launcher layout
-PRODUCT_PACKAGES += \
-    ShamuLayout
+#PRODUCT_PACKAGES += \
+#    ShamuLayout
 
 PRODUCT_PACKAGES += \
     bdAddrLoader
