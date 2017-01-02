391576e024843c19aa0a7d748b17a45acfe48ecc
Chris <chriscrawf95@gmail.com>
2017-01-02 17:17:10 -0500

    BoardConfig: Target typo in kernel source path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building
 TARGET_KERNEL_CONFIG := shamu_defconfig
-TARGET_KERNEL_SOURCE := kernel/moto/shamu
+TARGET_KERNEL_SOURCE := kernel/motorola/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
