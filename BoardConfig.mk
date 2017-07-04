cf30de84c728618c024a2363064dbdd7e32e396f
Lennart Wieboldt <lennart.1997@gmx.de>
2017-07-04 20:00:28 +0200

    Shamu: remove USE_CLANG_PLATFORM_BUILD
    * clang is used by default now
    
    Change-Id: I808396d35c1463c01f03df3bd6d299c80c2c98bc
    Signed-off-by: Lennart Wieboldt <lennart.1997@gmx.de>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,8 +129,6 @@ BOARD_HAS_AUDIO_DSP := true
 
 USE_DEVICE_SPECIFIC_CAMERA:= true
 
-USE_CLANG_PLATFORM_BUILD := true
-
 # Disable dex-preopt of prebuilts to save space.
 DONT_DEXPREOPT_PREBUILTS := true
 
