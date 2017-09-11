db19d9027dc95ec1c7fd473052d694c9fb6514b6
Pranav Vashi <neobuddy89@gmail.com>
2017-09-11 13:32:58 +0100

    shamu: Clean up NFC defines
    Dirty Unicorns edit by moepda as most of commit was
    already in place so only service & vendor needed adding
    
    Change-Id: I4526ce83a869aa0f43982c0f613ca4dccf1a8be7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: moepda <moepda@gmail.com>

--- device.mk
+++ device.mk
@@ -351,7 +351,8 @@ PRODUCT_PACKAGES += \
     nfc_nci.bcm2079x.default \
     NfcNci \
     Tag \
-    android.hardware.nfc@1.0-impl
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service
 
 # NFCEE access control
 PRODUCT_COPY_FILES += \
@@ -362,8 +363,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
-    device/motorola/shamu/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/motorola/shamu/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
+    device/motorola/shamu/nfc/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    device/motorola/shamu/nfc/libnfc-brcm-20795a10.conf:system/vendor/etc/libnfc-brcm-20795a10.conf
 
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
