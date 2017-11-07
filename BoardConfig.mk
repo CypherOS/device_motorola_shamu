d9f4ec0ec11e6c524241afd74745b8d5b02bd750
Artem Borisov <dedsa2002@gmail.com>
2017-11-07 20:39:17 +0300

    shamu: use 64-bit binder API
    AOSP and mainline are going towards removing old 32-bit binder API support.
    64-bit binder is 100% compatible with 32-bit kernel and userspace,
    so there is no reason for us to keep using the old solution anymore.
    We've switched to new API in our kernel, same thing should be applied to our userspace.
    
    Change-Id: I3c95bd7fbd023c5cb08e856b3a3889c03228e843

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,5 +154,8 @@ DEVICE_MATRIX_FILE := device/motorola/shamu/compatibility_matrix.xml
 # Recovery
 LZMA_RAMDISK_TARGETS := recovery
 
+# Binder API version
+TARGET_USES_64_BIT_BINDER := true
+
 # Enable workaround for slow rom flash
 BOARD_SUPPRESS_SECURE_ERASE := true
