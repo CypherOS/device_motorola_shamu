377f459c00ee9c19747253b9fcb223ca7aeb1dd5
Christian Oder <myself5@carbonrom.org>
2018-01-09 14:12:24 +0100

    shamu: Don't include prebuilt headers
    Change-Id: Idbdaca1133048a8c8187ee976bf2fc0a66bd18ff

--- device.mk
+++ device.mk
@@ -419,7 +419,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
-$(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
+# $(call inherit-product-if-exists, hardware/qcom/msm8x84/msm8x84.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x84/msm8x84-gpu-vendor.mk)
 
 # setup dm-verity configs.
