e237ecd09d152b8aa5cdab71da2d04aa40b2ded1
Chris <chris@cypheros.co>
2018-01-19 00:26:52 -0500

    shamu: Remove ShamuLayout
    Change-Id: Ic110894352be938316227dd5b563074149b6dc91

--- device.mk
+++ device.mk
@@ -255,10 +255,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
-# for launcher layout
-PRODUCT_PACKAGES += \
-    ShamuLayout
-
 PRODUCT_PACKAGES += \
     bdAddrLoader
 
