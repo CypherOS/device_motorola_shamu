LineageOS/android_device_huawei_angler

Declare location of device manifest
Bug: 36810913
Test: make and load on device.

Change-Id: I3bb81a8c1fc74b676e97616114eb7f6f5eea1fce

BoardConfig.mk
@@ -141,4 +141,6 @@ USE_CLANG_PLATFORM_BUILD := true
 
 TARGET_FS_CONFIG_GEN += device/huawei/angler/config.fs
 
+DEVICE_MANIFEST_FILE := device/huawei/angler/manifest.xml
+
 -include vendor/huawei/angler/BoardConfigVendor.mk

CypherOS/device_motorola_shamu

BoardConfig.mk
@@ -133,6 +133,8 @@ DONT_DEXPREOPT_PREBUILTS := true
 
 TARGET_FS_CONFIG_GEN += device/motorola/shamu/config.fs
 
+DEVICE_MANIFEST_FILE := device/motorola/shamu/manifest.xml
+
 -include vendor/motorola/shamu/BoardConfigVendor.mk
 
 # Enable workaround for slow rom flash