AICP/device_moto_shamu

shamu: Convert audio_effects.conf to XML
* Based on https://android.googlesource.com/device/google/taimen/+/ecf82951a431324bd129b31495043dcfa00cfb1f

Change-Id: I6bdbf127b0281ca0ad9401f208dad3a8ed73588f

device.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    device/moto/shamu/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    device/moto/shamu/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     device/moto/shamu/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     device/moto/shamu/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     device/moto/shamu/audio/audio_policy_volumes_drc.xml:system/etc/audio_policy_volumes_drc.xml \

CypherOS/device_motorola_shamu

device.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    device/motorola/shamu/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    device/motorola/shamu/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     device/motorola/shamu/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     device/motorola/shamu/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     device/motorola/shamu/audio/audio_policy_volumes_drc.xml:system/etc/audio_policy_volumes_drc.xml \